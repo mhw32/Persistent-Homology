61996f223b4e6f96cc757662034c909eb6fd7177
mhw32 <mike.wu@yale.edu>
2015-09-18 11:29:00 -0400

    Added all the requirements of the stuff

---
+++ testing.r
@@ -0,0 +1,86 @@
+# Author: Mike Wu
+# Playing around a little bit more with persistent homology.
+library(TDA)
+
+# -------------------------------------------------------------------------
+# TRIAL ONE : Given N simulations of n particle datasets, generate and overlay persistent diagrams. 
+
+# We need a function to generate data from a uniform circle.
+# Inputs : n [number to sample], r [radius of the circle], c [center of the circle].
+circle <- function(num=n, rad=r, center=c(0, 0)) {
+  # Define center of the radius.
+  x0     <- center[1] 
+  y0     <- center[2]
+  # Sample uniform points and put them on a point.
+  u      <- 2 * pi * runif(num)
+  # Return the circles. 
+  result <- rad * cbind(x = cos(u) + x0, y = sin(u) + y0)
+  return(result)
+}
+
+# Run the RIPS diagram for N independent simulation
+multiRIPS <- function(maxdimension, maxscale, N) {
+  for (i in 1:N) {
+    Circle <- circle(numparticle, radius, center)
+    Diag   <- ripsDiag(Circle, maxdimension, maxscale, library = "GUDHI", printProgress = FALSE)
+    # Use points so that we can overlay these RIPS diagrams.
+    par(new = TRUE)
+    plot(Diag$diagram, main = "RIPS Diagram")
+  }
+}
+
+# Example running setting
+maxdimension <- 1          # components and loops
+maxscale     <- 5          # limit of the filtration
+numparticle  <- 10         # number of samples to draw per circle
+radius       <- 2          # radius of circle
+center       <- c(0, 0)    # center of circle
+multiRIPS(maxdimension, maxscale, 100)
+
+# ---------------------------------------------------------
+# TRIAL TWO : Do the same thing with "Kernel Density" RIPS.
+
+# Before we can do any distance estimates, we need to construct a grid of points over which we will evaluate the functions.
+# Inputs : X is the sampled units from a circle
+makeGrid <- function(X, Xlim, Ylim, by) {
+  Xseq <- seq(from = Xlim[1], to = Xlim[2], by = by)
+  Yseq <- seq(from = Ylim[1], to = Ylim[2], by = by)
+  Grid <- expand.grid(Xseq, Yseq)
+  return(Grid)
+}
+
+# Example of creating a grid.
+numparticle <- 10
+radius      <- 2      
+center      <- c(0, 0)
+X           <- circle(numparticle, radius, center)
+Xlim        <- c(center[1] - radius - 1, center[2] + radius + 1)
+Ylim        <- c(center[1] - radius - 1, center[2] + radius + 1)
+by          <- 0.1
+Grid        <- makeGrid(X, Xlim, Ylim, by)
+
+
+multiKDE <- function(N, Xlim, Ylim, by, h=0.3) {
+  for (i in 1:N) {
+    Circle <- circle(numparticle, radius, center)
+    # Use gridDiag with the Kernal Density Estimator.
+    Diag <- gridDiag(X = Circle, FUN = kde, lim = cbind(Xlim, Ylim), by = by, 
+                     sublevel = FALSE, library = "Dionysus", printProgress = FALSE, h = h)
+    par(new = TRUE)
+    plot(Diag$diagram, main = "KDE Diagram")
+  }
+}
+
+# ----------------------------------------------------------------
+# TRIAL THREE : Do the same thing with "Distance to Measure" RIPS.
+multiDTM <- function(N, Xlim, Ylim, by, m0=0.1) {
+  for (i in 1:N) {
+    Circle <- circle(numparticle, radius, center)
+    # Use gridDiag with the Kernal Density Estimator.
+    Diag <- gridDiag(X = Circle, FUN = dtm, lim = cbind(Xlim, Ylim), by = by, 
+                     sublevel = FALSE, library = "Dionysus", printProgress = FALSE, m0 = h)
+    par(new = TRUE)
+    plot(Diag$diagram, main = "DTM Diagram")
+  }
+}
+
