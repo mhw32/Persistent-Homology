909e719f8ac7ae5b977d161940de6b42c1ffa29a
mhw32 <mike.wu@yale.edu>
2015-09-27 11:11:16 -0400

    Adding more KDE analysis

--- testing.r
+++ testing.r
@@ -7,21 +7,21 @@ library(TDA)
 
 # We need a function to generate data from a uniform circle.
 # Inputs : n [number to sample], r [radius of the circle], c [center of the circle].
-circle <- function(num=n, rad=r, center=c(0, 0)) {
+genCircle <- function(num=n, rad=r, center=c(0, 0)) {
   # Define center of the radius.
   x0     <- center[1] 
   y0     <- center[2]
   # Sample uniform points and put them on a point.
   u      <- 2 * pi * runif(num)
   # Return the circles. 
-  result <- rad * cbind(x = cos(u) + x0, y = sin(u) + y0)
+  result <- rad / 2 * cbind(x = cos(u) + x0, y = sin(u) + y0)
   return(result)
 }
 
 # Run the RIPS diagram for N independent simulation
 multiRIPS <- function(maxdimension, maxscale, N, K) {
   for (i in 1:N) {
-    Circle <- circle(K, radius, center)
+    Circle <- genCircle(K, radius, center)
     Diag   <- ripsDiag(Circle, maxdimension, maxscale, library = "GUDHI", printProgress = FALSE)
     # Use points so that we can overlay these RIPS diagrams.
     par(new = TRUE)
@@ -38,7 +38,7 @@ makeNoise <- function(mu, sigma, num) {
 # First way of adding noise. This shifts the actual points on the circle a little bit. 
 noisyRIPS <- function(maxdimension, maxscale, N, K, emu, esig) {
   for (i in 1:N) {
-    Circle <- circle(K, radius, center)
+    Circle <- genCircle(K, radius, center)
     Noise1 <- makeNoise(emu, esig, K)
     Noise2 <- makeNoise(emu, esig, K)
     # Combine the vectors with the noise
@@ -57,7 +57,7 @@ noisyRIPS <- function(maxdimension, maxscale, N, K, emu, esig) {
 # But it lets circles maintain its structure.
 noisy2RIPS <- function(maxdimension, maxscale, N, K, E) {
   for (i in 1:N) {
-    Circle <- circle(K, radius, center)
+    Circle <- genCircle(K, radius, center)
     Noise1 <- makeNoise(center[1], radius/2, E)
     Noise2 <- makeNoise(center[1], radius/2, E)
     Noise <- cbind(Noise1, Noise2)
@@ -76,12 +76,31 @@ noisy2RIPS <- function(maxdimension, maxscale, N, K, E) {
 maxdimension <- 1          # components and loops
 maxscale     <- 5          # limit of the filtration
 numparticle  <- 10         # number of samples to draw per circle
-radius       <- 2          # radius of circle
+radius       <- 5          # radius of circle
 center       <- c(0, 0)    # center of circle
 multiRIPS(maxdimension, maxscale, 10, 250)
 
-# ---------------------------------------------------------
-# TRIAL TWO : Do the same thing with "Kernel Density" RIPS.
+# ----------------------------------------------------------------
+# TRIAL THREE : Do the same thing with "Distance to Measure" RIPS.
+multiDTM <- function(N, K, Xlim, Ylim, by, m0=0.1) {
+  for (i in 1:N) {
+    Circle <- genCircle(K, radius, center)
+    # Use gridDiag with the Kernal Density Estimator.
+    Diag <- gridDiag(X = Circle, FUN = dtm, lim = cbind(Xlim, Ylim), by = by, 
+                     sublevel = FALSE, library = "Dionysus", printProgress = FALSE, m0 = m0)
+    par(new = TRUE)
+    plot(Diag$diagram, main = paste("DTM Diagram", "( N =", toString(N), ", n =", toString(K), ")"))
+  }
+}
+
+# Reformatting Function after reading TDA tutorial more carefully.
+# ----------------------------------------------------------------
+
+singleKDE <- function(Circle, Grid, h=0.3) {
+  band=bootstrapBand(X=Circle, FUN=kde, Grid=Grid, B=100, parallel=FALSE, alpha=0.1, h=h)
+  Diag = gridDiag( X=X, FUN=kde, h=0.3, lim=cbind(Xlim,Ylim), by=by, sublevel=FALSE, library="Dionysus", printProgress=FALSE )$diagram
+  return(list(first=band, second=Diag))
+}
 
 # Before we can do any distance estimates, we need to construct a grid of points over which we will evaluate the functions.
 # Inputs : X is the sampled units from a circle
@@ -92,37 +111,19 @@ makeGrid <- function(X, Xlim, Ylim, by) {
   return(Grid)
 }
 
-# Example of creating a grid.
-numparticle <- 10
-radius      <- 2      
-center      <- c(0, 0)
-X           <- circle(numparticle, radius, center)
-Xlim        <- c(center[1] - radius - 1, center[2] + radius + 1)
-Ylim        <- c(center[1] - radius - 1, center[2] + radius + 1)
-by          <- 0.1
-Grid        <- makeGrid(X, Xlim, Ylim, by)
-
-multiKDE <- function(N, K, Xlim, Ylim, by, h=0.3) {
+multiKDE <- function(N, K, Xlim=NULL, Ylim=NULL, by=0.065, rad=5, center=c(0,0), h=0.3, main="KDE Diagram") {
+  par(new = TRUE)
   for (i in 1:N) {
-    Circle <- circle(K, radius, center)
-    # Use gridDiag with the Kernal Density Estimator.
-    Diag <- gridDiag(X = Circle, FUN = kde, lim = cbind(Xlim, Ylim), by = by, 
-                     sublevel = FALSE, library = "Dionysus", printProgress = FALSE, h = h)
-    par(new = TRUE)
-    plot(Diag$diagram, main = paste("KDE Diagram", "( N =", toString(N), ", n =", toString(K), ")"))
-  }
-}
-
-# ----------------------------------------------------------------
-# TRIAL THREE : Do the same thing with "Distance to Measure" RIPS.
-multiDTM <- function(N, K, Xlim, Ylim, by, m0=0.1) {
-  for (i in 1:N) {
-    Circle <- circle(K, radius, center)
-    # Use gridDiag with the Kernal Density Estimator.
-    Diag <- gridDiag(X = Circle, FUN = dtm, lim = cbind(Xlim, Ylim), by = by, 
-                     sublevel = FALSE, library = "Dionysus", printProgress = FALSE, m0 = m0)
-    par(new = TRUE)
-    plot(Diag$diagram, main = paste("DTM Diagram", "( N =", toString(N), ", n =", toString(K), ")"))
+    Circle <- genCircle(K, rad, center)
+    if (is.null(Xlim)) {
+      Xlim <- c(center[1] - rad - 1, center[1] + rad + 1)
+    }
+    if (is.null(Ylim)) {
+      Ylim <- c(center[2] - rad - 1, center[2] + rad + 1) 
+    }
+    grid <- makeGrid(Circle, Xlim, Ylim, by)
+    result <- singleKDE(Circle, grid, h)
+    plot(result$second, band=2*result$first$width, main=main)
   }
 }
 
