eb0555702e8289686aaacc2a1b122c7e98b7c5b5
mhw32 <mike.wu@yale.edu>
2015-09-27 12:54:04 -0400

    Removing extra storage of images, added gitignore, added refractoring of code

--- testing.r
+++ testing.r
@@ -3,7 +3,7 @@
 library(TDA)
 
 # -------------------------------------------------------------------------
-# TRIAL ONE : Given N simulations of n particle datasets, generate and overlay persistent diagrams. 
+# Given N simulations of n particle datasets, generate and overlay persistent diagrams. 
 
 # We need a function to generate data from a uniform circle.
 # Inputs : n [number to sample], r [radius of the circle], c [center of the circle].
@@ -14,9 +14,17 @@ genCircle <- function(num=n, rad=r, center=c(0, 0)) {
   # Sample uniform points and put them on a point.
   u      <- 2 * pi * runif(num)
   # Return the circles. 
-  result <- rad / 2 * cbind(x = cos(u) + x0, y = sin(u) + y0)
+  result <- rad * cbind(x = cos(u) + x0, y = sin(u) + y0)
   return(result)
 }
+
+# Example running setting
+maxdimension <- 1          # components and loops
+maxscale     <- 5          # limit of the filtration
+numparticle  <- 10         # number of samples to draw per circle
+radius       <- 5          # radius of circle
+center       <- c(0, 0)    # center of circle
+multiRIPS(maxdimension, maxscale, 10, 250)
 
 # Run the RIPS diagram for N independent simulation
 multiRIPS <- function(maxdimension, maxscale, N, K) {
@@ -29,59 +37,8 @@ multiRIPS <- function(maxdimension, maxscale, N, K) {
   }
 }
 
-# Adds gaussian noise
-makeNoise <- function(mu, sigma, num) {
-  result <- rnorm(num, mean = mu, sd = sigma)
-  return(result)
-}
-
-# First way of adding noise. This shifts the actual points on the circle a little bit. 
-noisyRIPS <- function(maxdimension, maxscale, N, K, emu, esig) {
-  for (i in 1:N) {
-    Circle <- genCircle(K, radius, center)
-    Noise1 <- makeNoise(emu, esig, K)
-    Noise2 <- makeNoise(emu, esig, K)
-    # Combine the vectors with the noise
-    Circle[, 1] = Circle[, 1] + Noise1
-    Circle[, 2] = Circle[, 2] + Noise2
-    # Add some gaussian noise to the circle.
-    Diag   <- ripsDiag(Circle, maxdimension, maxscale, library = "GUDHI", printProgress = FALSE)
-    # Use points so that we can overlay these RIPS diagrams.
-    par(new = TRUE)
-    plot(Diag$diagram, main = paste("RIPS Diagram", "( N =", toString(N), ", n =", toString(K), "noise = Gaussian(", toString(emu), ",", toString(esig), ")", ")"
-))
-  }
-}
-
-# This adds noise a second way. This adds additional points themselves to the circle.
-# But it lets circles maintain its structure.
-noisy2RIPS <- function(maxdimension, maxscale, N, K, E) {
-  for (i in 1:N) {
-    Circle <- genCircle(K, radius, center)
-    Noise1 <- makeNoise(center[1], radius/2, E)
-    Noise2 <- makeNoise(center[1], radius/2, E)
-    Noise <- cbind(Noise1, Noise2)
-    # Combine the vectors with the noise
-    Circle <- rbind(Circle, Noise)
-    # Add some gaussian noise to the circle.
-    Diag   <- ripsDiag(Circle, maxdimension, maxscale, library = "GUDHI", printProgress = FALSE)
-    # Use points so that we can overlay these RIPS diagrams.
-    par(new = TRUE)
-    plot(Diag$diagram, main = paste("RIPS Diagram", "( N =", toString(N), ", n =", toString(K), "noise = Gaussian(", toString(center[1]), ",", toString(radius/2), ")", ")"
-    ))
-  }
-}
-
-# Example running setting
-maxdimension <- 1          # components and loops
-maxscale     <- 5          # limit of the filtration
-numparticle  <- 10         # number of samples to draw per circle
-radius       <- 5          # radius of circle
-center       <- c(0, 0)    # center of circle
-multiRIPS(maxdimension, maxscale, 10, 250)
-
-# ----------------------------------------------------------------
-# TRIAL THREE : Do the same thing with "Distance to Measure" RIPS.
+# --------------------------------------------------
+# Do the same thing with "Distance to Measure" RIPS.
 multiDTM <- function(N, K, Xlim, Ylim, by, m0=0.1) {
   for (i in 1:N) {
     Circle <- genCircle(K, radius, center)
@@ -111,10 +68,29 @@ makeGrid <- function(X, Xlim, Ylim, by) {
   return(Grid)
 }
 
-multiKDE <- function(N, K, Xlim=NULL, Ylim=NULL, by=0.065, rad=5, center=c(0,0), h=0.3, main="KDE Diagram") {
+# We may want to visualize the Topo plot.
+plot3D <- function(X, Xlim, Ylim, by=0.065, h=0.3, main="3D KDE Diagram") {
+  Xseq <- seq(from = Xlim[1], to = Xlim[2], by = by)
+  Yseq <- seq(from = Ylim[1], to = Ylim[2], by = by)
+  # Cast the grid, but we also want to expose Xseq,Yseq
+  Grid <- expand.grid(Xseq, Yseq)
+  # Run the KDE ontop of the grid
+  KDE= kde(X=X, Grid=Grid, h=h)
+  persp(Xseq,Yseq,matrix(KDE, ncol=length(Yseq), nrow=length(Xseq)),
+        xlab="", ylab="", zlab="", theta=-20, phi=35, ltheta=50, col=2, border=NA,
+        main=main, d=0.5, scale=FALSE, expand=3, shade=0.9)
+}
+
+multiKDE <- function(N, K, Xlim=NULL, Ylim=NULL, by=0.065, rad=5, center=c(0,0), h=0.3, noise=0, main="KDE Diagram") {
   par(new = TRUE)
   for (i in 1:N) {
     Circle <- genCircle(K, rad, center)
+    if (noise == 1) {
+      Circle <- addRandomNoise(Circle, 0, 1)
+    } else if (noise == 2) {
+      Circle <- addRandomPoints(Circle, rad, center, 100)
+    }
+    
     if (is.null(Xlim)) {
       Xlim <- c(center[1] - rad - 1, center[1] + rad + 1)
     }
@@ -125,5 +101,35 @@ multiKDE <- function(N, K, Xlim=NULL, Ylim=NULL, by=0.065, rad=5, center=c(0,0),
     result <- singleKDE(Circle, grid, h)
     plot(result$second, band=2*result$first$width, main=main)
   }
+}
+
+# Functions to add noise to data.
+# -------------------------------
+# Adds gaussian noise
+makeNoise <- function(mu, sigma, num) {
+  result <- rnorm(num, mean = mu, sd = sigma)
+  return(result)
+}
+
+# Provided any circle, this shifts around the points a little bit. 
+addRandomNoise <- function(Circle, emu=0, esig=1) {
+  K <- length(Circle)
+  Noise1 <- makeNoise(emu, esig, K)
+  Noise2 <- makeNoise(emu, esig, K)
+  # Combine the vectors with the noise.
+  Circle[, 1] = Circle[, 1] + Noise1
+  Circle[, 2] = Circle[, 2] + Noise2
+  # Return a circular object.
+  return(Circle)
+}
+
+# This generates random points to add to the Circle vector. 
+addRandomPoints <- function(Circle, radius, center, E=100) {
+  Noise1 <- makeNoise(center[1], radius/2, E)
+  Noise2 <- makeNoise(center[2], radius/2, E)
+  Noise  <- cbind(Noise1, Noise2)
+  # Combine the vectors with the noise
+  Circle <- rbind(Circle, Noise)
+  return(Circle)
 }
 
