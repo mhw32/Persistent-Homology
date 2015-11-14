6f8906c1088c167e4f42dcbef0f5d334ff2fc646
mhw32 <mike.wu@yale.edu>
2015-11-13 20:47:40 -0500

    Added FNN library for kde2d

--- distribution.r
+++ distribution.r
@@ -1,12 +1,13 @@
 # Direct Distribution Functions.
 # Mike Wu
 
-# Method: Parametrize the data. Because I can't really find a good multivariate non-parametric test, I can try to project the 2-dimension data into 1-dimension. I want to preserve relative distances between points. We note that by construction, the points in the persistence diagram follow a diagonal line. Therefore, we can rotate the plot and put the diagonal in the x-axis. (birth + death) / 2 and (birth - death) / 2. 
+# Method: Parametrize the data. Because I can't really find a good multivariate non-parametric test, I can try to project the 2-dimension data into 1-dimension. I want to preserve relative distances between points. We note that by construction, the points in the persistence diagram follow a diagonal line. Therefore, we can rotate the plot and put the diagonal in the x-axis. (birth + death) / 2 and (birth - death) / 2.
 # This is actually a matrix transformation. We also need to scale the standard deviation properly: A <- 1/sqrt(2)*matrix(c(1,1,1,-1),2,2). This should preserve all distances and essentially equals SVD. Then we can remove the 2nd dimension and be left with a scaled dimension. This is the first principle component!
 
 source('tools.r')
+library(FNN)
 
-# 2-to-1 Dimensionality reduction based on 
+# 2-to-1 Dimensionality reduction based on
 reduce <- function(mat) {
   A <- 1/sqrt(2)*matrix(c(1,1,1,-1),2,2)
   z <- mat %*% A # Reparametrization
@@ -18,12 +19,12 @@ distribDimStat <- function(set, dim) {
   setnum <- length(set)
   reducedset <- lapply(1:setnum, function(i) {
     # Reduce specific dimension(s).
-    reduce(sliceDim(set[[i]], dim)) 
+    reduce(sliceDim(set[[i]], dim))
   })
   return(reducedset)
 }
 
-# Contour Test. This tests for the different in densities of the kernel density estimates of two things. 
+# Contour Test. This tests for the different in densities of the kernel density estimates of two things.
 contourDimStat <- function(set, dim) {
   setnum <- length(set)
   sliceset <- lapply(1:setnum, function(i) {
@@ -35,5 +36,3 @@ contourDimStat <- function(set, dim) {
   })
   return(sliceset)
 }
-
-
