f974dd6bae80422dba01d65d77b5965d4302ff6e
mhw32 <mike.wu.mike@gmail.com>
2015-11-14 23:45:19 -0500

    gitignore random files

--- run_voronoi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#BSUB -W 20:10                # wall-clock time (hrs:mins)
-#BSUB -e cluster-errors.%J    # error file name in which %J is replaced by the job ID
-#BSUB -o clutser-output.%J    # output file name in which %J is replaced by the job ID
-#BSUB -n 1
-
-# Run R non-interactively.
-R CMD BATCH run_voronoi.R
-
