e78cb71c0ec979ee1247d2095c3b5ae338f4ab04
mhw32 <mike.wu@yale.edu>
2015-11-30 22:30:20 -0500

    scped the file over and now I'm working on getting eagle and setting up some jobs

---
+++ simulations/eagle.R
@@ -0,0 +1,20 @@
+
+
+# source("http://bioconductor.org/biocLite.R")
+# biocLite("rhdf5")
+library(rhdf5)
+library(rgl)
+library(scatterplot3d)
+
+
+
+#d1 = t(h5read("Output_Eagle_Volume.hdf5", "P1/ParticlePositions"))
+d1 = t(h5read("Output_Eagle_Volume.hdf5", "P1/SubhaloPositions"))
+
+
+index0 = sample(1:nrow(d1),10000,replace = FALSE)
+plot3d(d1[index0,], pch = 19, tick.marks = FALSE, xlab = "", ylab = "",zlab = "", cex.symbols = .01)
+title(paste(length(index0), " of ", nrow(d1), " particles", sep = ""))
+
+
+apply(d1,2,fivenum)
