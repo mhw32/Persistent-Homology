349dbf19d7bde9a61635ccdd293b33d891d04c10
mhw32 <mike.wu@yale.edu>
2015-12-19 11:09:56 -0500

    typo in run_full_eagle

--- run_full_eagle.r
+++ run_full_eagle.r
@@ -7,10 +7,10 @@ cdm <- load_CDM()
 wdm <- load_WDM()
 # Plot these actual diagrams
 png(filename="./saved_states/cdmplot.png")
-scatterplot3d(cdm, pch=19, cex.symbols=0.005)
+scatterplot3d(cdm, pch=19, cex.symbols=0.001)
 dev.off()
 png(filename="./saved_states/wdmplot.png")
-scatterplot3d(cdm, pch=19, cex.symbols=0.005)
+scatterplot3d(wdm, pch=19, cex.symbols=0.001)
 dev.off()
 # Settings for generating diagrams
 res <- 2
@@ -22,9 +22,10 @@ wdm_diag <- gridDiag(wdm, dtm, lim=cbind(boxlim, boxlim, boxlim), by=res, sublev
 saveRDS(cdm_diag, "./saved_states/full_cdm_diag.rds")
 saveRDS(wdm_diag, "./saved_states/full_wdm_diag.rds")
 # Run the single test we need  -- global kde test.
-pval <- ks::kde.test(cdm_diag, wdm_diag)$pvalue
+pval <- ks::kde.test(cdm_diag$diagram, wdm_diag$diagram)$pvalue
 print(paste("Full global kde test result:",pval,sep=" "))
+# Full global kde test result: 0.6944359
 # Run the local kde test.
-localdiagplot(cdm_diag, wdm_diag, 0, "CDM/WDM 0 dim", "./saved_states/localks-0-dim.pdf")
-localdiagplot(cdm_diag, wdm_diag, 1, "CDM/WDM 1 dim", "./saved_states/localks-1-dim.pdf")
-localdiagplot(cdm_diag, wdm_diag, 2, "CDM/WDM 2 dim", "./saved_states/localks-2-dim.pdf")
+localdiagplot(cdm_diag$diagram, wdm_diag$diagram, 0, "CDM/WDM 0 dim", "./saved_states/localks-0-dim.png")
+localdiagplot(cdm_diag$diagram, wdm_diag$diagram, 1, "CDM/WDM 1 dim", "./saved_states/localks-1-dim.png")
+localdiagplot(cdm_diag$diagram, wdm_diag$diagram, 2, "CDM/WDM 2 dim", "./saved_states/localks-2-dim.png")
