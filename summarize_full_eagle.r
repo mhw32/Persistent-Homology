0b9e04f55ee386ba5f824260e2134d454f3bf9a5
mhw32 <mike.wu.mike@gmail.com>
2016-03-31 20:01:13 -0400

    edits to summary scripts

--- summarize_full_eagle.r
+++ summarize_full_eagle.r
@@ -5,13 +5,13 @@ source("summarize.r")
 cdm_source <- "./saved_states/full_cdm_diag.rds"
 wdm_source <- "./saved_states/full_wdm_diag.rds"
 
-cdm_diag <- readRDS(cdm_source)
-wdm_diag <- readRDS(wdm_source)
+cdm_diag <- readRDS(cdm_source)$diagram
+wdm_diag <- readRDS(wdm_source)$diagram
 
-eulerPlot(cdm_diag, main='CDM Full Euler Characteristic', path='./saved_states/real_data/cut_diags/')
-eulerPlot(wdm_diag, main='WDM Full Euler Characteristic', path='./saved_states/real_data/cut_diags/')
+eulerPlot(cdm_diag, main='CDM Full Euler Characteristic', path='./saved_states/real_data/cut_diags/cdm_full_euler.png')
+eulerPlot(wdm_diag, main='WDM Full Euler Characteristic', path='./saved_states/real_data/cut_diags/wdm_full_euler.png')
 
 for (i in 1:3) {
-  silhouettePlot(cdm_diag, dim=i, main=paste('CDM Full Silhouette Dimension', i), path='./saved_states/real_data/cut_diags/')
-  silhouettePlot(wdm_diag, dim=i, main=paste('WDM Full Silhouette Dimension', i), path='./saved_states/real_data/cut_diags/')
+  silhouettePlot(cdm_diag, dim=i, main=paste('CDM Full Silhouette Dimension', i), path=paste('./saved_states/real_data/cut_diags/cdm_full_silh_', i, '.png', sep=""))
+  silhouettePlot(wdm_diag, dim=i, main=paste('WDM Full Silhouette Dimension', i), path=paste('./saved_states/real_data/cut_diags/wdm_full_silh_', i, '.png', sep=""))
 }
--- summarize_sliced_eagle.r
+++ summarize_sliced_eagle.r
@@ -12,12 +12,12 @@ for (i in 2:4) {
     cdmdiag <- cdmobj[[j]]
     wdmdiag <- wdmobj[[j]]
 
-    eulerPlot(cdmdiag, main=paste('CDM Slice (', j, '/', num, ') Euler Characteristic'), path=savepath)
-    eulerPlot(wdmdiag, main=paste('WDM Slice (', j, '/', num, ') Euler Characteristic'), path=savepath)
+    eulerPlot(cdmdiag, main=paste('CDM Slice (', j, '/', num, ') Euler Characteristic'), path=paste(savepath, "cdm_euler_", j, "_out_of_", num, ".png", sep=""))
+    eulerPlot(wdmdiag, main=paste('WDM Slice (', j, '/', num, ') Euler Characteristic'), path=paste(savepath, "wdm_euler_", j, "_out_of_", num, ".png", sep=""))
 
     for (k in 1:3) {
-      silhouettePlot(cdmdiag, dim=k, main=paste('CDM Slice (', j, '/', num, ')  Silhouette Dimension', k), path=savepath)
-      silhouettePlot(wdmdiag, dim=k, main=paste('WDM Slice (', j, '/', num, ')  Silhouette Dimension', k), path=savepath)
+      silhouettePlot(cdmdiag, dim=k, main=paste('CDM Slice (', j, '/', num, ')  Silhouette Dimension', k), path=paste(savepath, "cdm_silh_", j, "_out_of_", num, "_dim_", k, ".png", sep=""))
+      silhouettePlot(wdmdiag, dim=k, main=paste('WDM Slice (', j, '/', num, ')  Silhouette Dimension', k), path=paste(savepath, "wdm_silh_", j, "_out_of_", num, "_dim_", k, ".png", sep=""))
     }
   }   
 }
