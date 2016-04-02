fe5f6364fe21d5703b054805e9f71b9c41ba7e62
mhw32 <mike.wu@yale.edu>
2016-04-02 18:38:58 -0400

    Fixed python/R syntax error

--- run_tests.r
+++ run_tests.r
@@ -4,7 +4,7 @@ source('process_eagle.r')
 
 source <- "/home/fas/cisewski/mhw32/scratch/homology/saved_states/real_data/cut_diags/"
 
-for i in (2:4):
+for (i in 2:4) {
   cdm <- readRDS(paste(source, "cdm", i, ".rds", sep=""))
   wdm <- readRDS(paste(source, "wdm", i, ".rds", sep=""))
   bigset <- vector('list', 1)
@@ -28,4 +28,4 @@ for i in (2:4):
   bigset <- vector('list', 1)
   bigset[[1]] <- wdm
   test_wrapper(bigset, cdm, paste("Split", i, "CDMbaseNormTrue", sep=""), norm=TRUE, run_base=FALSE)
-
+}
