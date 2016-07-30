9922c3c86338d33862a00b3855502b76f6c0ce65
mhw32 <me@mikewuis.me>
2016-07-30 19:38:17 +0100

    updating r function

--- run_cube_compare.r
+++ run_cube_compare.r
@@ -3,11 +3,12 @@ source("distance.r")
 source("tools.r")
 
 dists <- array(0,dim=c(4, 64, 3))
-norm <- False
+norm <- TRUE
 
 for (i in 1:4) {
-  cdm_slices <- readRDS(paste('cdm_diags_', i, '.rds', sep=''))
-  wdm_slices <- readRDS(paste('wdm_diags_', i, '.rds', sep=''))
+  print(i)
+  cdm_slices <- readRDS(paste('saved_states/wdm_cdm_rds/cdm_diags_', i, '.rds', sep=''))
+  wdm_slices <- readRDS(paste('saved_states/wdm_cdm_rds/wdm_diags_', i, '.rds', sep=''))
   for (l in 1:i^3) {
     cdm_diag <- cleanDiag(cdm_slices[[l]])
     wdm_diag <- cleanDiag(wdm_slices[[l]])
@@ -18,10 +19,14 @@ for (i in 1:4) {
     }
 
     # get the bottle neck distances.
-    dists[i,l,1] <- bottleneckDist(cdm_diag, wdm_diag, dimension=1)
-    dists[i,l,2] <- bottleneckDist(cdm_diag, wdm_diag, dimension=2)
-    dists[i,l,3] <- bottleneckDist(cdm_diag, wdm_diag, dimension=3)
+    dists[i,l,1] <- bottleneckDist(cdm_diag, wdm_diag, dimension=0)
+    dists[i,l,2] <- bottleneckDist(cdm_diag, wdm_diag, dimension=1)
+    dists[i,l,3] <- bottleneckDist(cdm_diag, wdm_diag, dimension=2)
   }
 }
 
-saveRDS(dists, file='outputs/dists/dists.rds')
+if (norm) {
+  saveRDS(dists, file='saved_states/wdm_cdm_rds/dists-norm.rds')
+else {
+  saveRDS(dists, file='saved_states/wdm_cdm_rds/dists.rds')
+}
