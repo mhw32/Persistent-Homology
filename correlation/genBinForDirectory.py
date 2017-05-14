c1c36ee5de2a3bc066ed70005afb8d614ce99f75
mhw32 <me@mikewuis.me>
2017-05-14 00:39:48 -0700

    naming issues in correlation script

--- correlation/genBinForDirectory.py
+++ correlation/genBinForDirectory.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 import os
 import argparse
 import numpy as np
-from translate import np_to_bin
+from translate import np_to_bin, np_to_rds
 
 
 def make_parser():
@@ -14,6 +14,7 @@ def make_parser():
   )
   parser.add_argument('dataDir', type=str, help='path to input data')
   parser.add_argument('outDir', type=str, help='path to output data')
+  parser.add_argument('--rds', action='store_true')
   args = parser.parse_args()
   return args
 
@@ -23,9 +24,19 @@ dataDir = os.listdir(args.dataDir)
 for dataFile in dataDir:
   dataFileArr = dataFile.split('.')
   if dataFileArr[-1] == 'npy':
-    dataFileArr[-1] = 'bin'
-    newDataFile = '.'.join(dataFileArr)
-    np_to_bin(
-      os.path.join(args.dataDir, dataFile), 
-      outputfile=os.path.join(args.outDir, newDataFile),
-    )
+    if args.rds:
+      dataFileArr[-1] = 'rds'
+      newDataFile = '.'.join(dataFileArr)
+      objectName = 'cdm' if cdm in dataFile else 'wdm'
+      np_to_rds(
+        os.path.join(args.dataDir, dataFile), 
+        os.path.join(args.outDir, newDataFile),
+        objectName,
+      )
+    else:
+      dataFileArr[-1] = 'bin'
+      newDataFile = '.'.join(dataFileArr)
+      np_to_bin(
+        os.path.join(args.dataDir, dataFile), 
+        outputfile=os.path.join(args.outDir, newDataFile),
+      )
--- correlation/runEagleCorrTest.py
+++ correlation/runEagleCorrTest.py
@@ -27,7 +27,7 @@ def run_corr_test(dataDir, outDir, norm=False):
   all_cdm_corr = np.zeros((4, 64))
   all_wdm_corr = np.zeros((4, 64))
 
-  for i in np.arange(2, 5)[::-1]:
+  for i in np.arange(1, 5)[::-1]:
     print('Operating on set %d' % i)
     cdm_corr, wdm_corr = corr_simu_test_suite(
       os.path.join(dataDir, 'cdm_coords_%d.rds' % i),
@@ -51,7 +51,7 @@ def run_corr_func_test(dataDir, outDir, norm=False):
   all_cdm_func = np.zeros((4, 64, 100))
   all_wdm_func = np.zeros((4, 64, 100))
 
-  for i in np.arange(2, 5)[::-1]:
+  for i in np.arange(1, 5)[::-1]:
     print('Operating on set %d' % i)
     cdm_func, wdm_func = corr_simu_func_suite(
       os.path.join(dataDir, 'cdm_coords_%d.rds' % i),
